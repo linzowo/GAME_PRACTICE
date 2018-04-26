885a903cae7a8015ad7eec4d3b42ad79f3e08854
linzowo <33919971+linzowo@users.noreply.github.com>
2018-04-26 17:58:05 +0800

    Update shoot_game.py

--- shoot_game/shoot_game.py
+++ shoot_game/shoot_game.py
@@ -8,6 +8,7 @@ from ship import Ship
 from settings import Settings
 from diamonds import Diamonds
 from game_stats import Stats
+from play_button import Button
 
 def run_game():
     pygame.init()
@@ -19,13 +20,14 @@ def run_game():
     diamonds = Diamonds(screen,al_settings)
     ship = Ship(screen,al_settings)
     bullets = Group()
+    button = Button(screen,"play")
 
     while True:
-        gf.check_events(ship,al_settings,bullets,screen)
+        gf.check_events(ship,al_settings,bullets,screen,button,stats)
         if stats.game_active:
             ship.update()
             gf.update_bullet(diamonds,bullets,stats)
             diamonds.update()
-        gf.update_screen(screen,ship,diamonds,al_settings,bullets)
+        gf.update_screen(screen,ship,diamonds,al_settings,bullets,stats,button)
 
 run_game()
